80aaee668a373b0a29386facf78161cc76ef01c5
student <i.i.f@mail.ru>
2022-05-04 22:50:33 +0800

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringcoursework1/SkyproSpringCoursework1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcoursework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCoursework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCoursework1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringcoursework1/SkyproSpringCoursework1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcoursework1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringCoursework1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
